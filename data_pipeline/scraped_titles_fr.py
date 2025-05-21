bef0e2ab30b0d7f5586ec33533184f96559b29ad
aw814 <annie010814@mail.com>
2025-05-21 10:13:00 -0700

    uploading code for WikiGap paper submission

---
+++ data_pipeline/scraped_titles_fr.py
@@ -0,0 +1,10 @@
+# Auto-generated file containing (English, Target language) topic tuples
+
+en_tgt_title_pairs = [
+    ('Philippine adobo', 'Adobo philippin'),
+    ('Wiener schnitzel', 'Escalope à la viennoise'),
+    ('Peking duck', 'Canard laqué de Pékin'),
+    ('Paella', 'Paella'),
+    ('Injera', 'Injera'),
+    ('Oolong', 'Oolong')
+]
---
+++ data_pipeline/scraped_titles_zh.py
@@ -0,0 +1,10 @@
+# Auto-generated file containing (English, Target language) topic tuples
+
+en_tgt_title_pairs = [
+    ('Philippine adobo', '菲律賓阿多波'),
+    ('Wiener schnitzel', '维也纳炸牛排'),
+    ('Peking duck', '北京烤鸭'),
+    ('Paella', '西班牙鐵鍋飯'),
+    ('Injera', '英杰拉'),
+    ('Oolong', '乌龙茶')
+]
